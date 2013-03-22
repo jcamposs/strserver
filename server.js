c651173b13ce2cdd57a9c67ebede86b9bdb9b488
Santiago Carot Nemesio <sancane@gmail.com>
2013-03-22 16:48:12 +0100

    Move authorization code out the main file

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-var express = require('express');
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , authorize = require('./lib/authorize.js');
+
 var app = express();
 
 // Configuration
@@ -44,24 +50,3 @@ var workspace = io
   });
 
 server.listen(9000);
-
-function print_headers(headers) {
-console.log("BEGIN HEADERS:");
-  for (var h in headers)
-    console.log(h + ": " + headers[h]);
-console.log("END HEADERS:")
-}
-
-function authorize(handshakeData, callback) {
-  for (var p in handshakeData) {
-    if (p == "headers")
-      print_headers(handshakeData[p]);
-    else if (p == "address")
-      console.log("Client: " + handshakeData[p].address + ":" + handshakeData[p].port);
-    else
-      console.log(p + ": " + handshakeData[p]);
-  }
-
-  //TODO: Check if this user is logged using the cookie set in headers
-  callback(null, true);
-}
